eb35c2df550684b40719b4e7f33a3b2d13f72d85
Nik Nordquist <nnordquist3@gmail.com>
2024-07-30 17:06:27 -0500

    Converted .csv database to .json format

--- triviascript.js
+++ triviascript.js
@@ -6,4 +6,12 @@ playButton.addEventListener("click", () => {
     document.getElementById("reveal-player").style.display = "block";
 });
 
-
+let secretPlayer;
+const playerDB = fetch('./playerDatabase.json')
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(playerDB) {
+        secretPlayer = playerDB[Math.floor(Math.random() * playerDB.length)]
+        document.getElementById('player-name').innerText = secretPlayer.answer;
+    });
