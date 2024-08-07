15bb641431fc26720fa9ff9adc05788a6fbf9caf
Nik Nordquist <nnordquist3@gmail.com>
2024-08-07 17:36:07 -0500

    Developed skeleton guessing mechanic

--- triviascript.js
+++ triviascript.js
@@ -12,9 +12,13 @@ let playerPosition;
 let playerValue;
 
 let guess = 1;
+let clubFound = 0;
+let nationFound = 0;
 
 playButton.addEventListener("click", () => {
     guess = 1;
+    clubFound = 0;
+    nationFound = 0;
     document.getElementById("newgame").style.display = "none";
     document.getElementById("gamehud").style.display = "block";
     document.getElementById("player-title").style.display = "none";
@@ -53,15 +57,24 @@ submitButton.addEventListener("click", (event) => {
     event.preventDefault();
     if ((document.getElementById("userGuess").value).toLowerCase() == secretPlayer.name.toLowerCase()) {
         document.getElementById('searchbar').style.display = "none";
-        win();
+        reveal();
     } else {
-    document.getElementById(`guess${guess}`).innerHTML = document.getElementById("userGuess").value;
-    document.getElementById('userGuess').value = "";
+        /* TODO: Add in hint functionality */
+        document.getElementById(`guess${guess}`).innerHTML = document.getElementById("userGuess").value;
+        document.getElementById('userGuess').value = "";
+        if (guess === 6) {
+            document.getElementById("hint1").innerHTML = `Market Value: £${playerValue}m`;
+        } else if (guess === 9) {
+            document.getElementById("hint2").innerHTML = secretPlayer.club;
+            document.getElementById('club').src = `./img-clubs/${secretPlayer.club}.png`;
+        } else if (guess === 10) {
+            reveal();
+        }
     }
     guess++;
 }); 
 
-function win() {
+function reveal() {
     document.getElementById('player-name').innerText = playerName;
     document.getElementById('player-image').src = playerImage;
     document.getElementById('club').src = playerClub; 
