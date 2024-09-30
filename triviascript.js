205c168376155b6d0ed5de5a0ff56a2694fcadc0
Nik Nordquist <nnordquist3@gmail.com>
2024-09-30 14:13:29 -0500

    Whitespace in guessbox now ignored

--- triviascript.js
+++ triviascript.js
@@ -156,7 +156,7 @@ function autocomplete(inp, arr) {
         /*for each item in the array...*/
         for (i = 0; i < arr.length; i++) {
             /*check if the item starts with the same letters as the text field value:*/
-            if (arr[i].answer.toUpperCase().includes(val.toUpperCase())) {
+            if (arr[i].answer.toUpperCase().includes(val.toUpperCase().trim())) {
                 /*create a DIV element for each matching element:*/
                 b = document.createElement("DIV");
                 /*make the matching letters bold:*/
